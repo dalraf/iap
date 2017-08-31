faa322fe88a66da252736a0e8eb5029d15457fbe
Daniel Fonseca Alves <dalrafgmail.com>
2017-08-31 09:53:48 -0300

    Arquivos principais definidos

--- giap/views.py
+++ giap/views.py
@@ -4,3 +4,6 @@ from __future__ import unicode_literals
 from django.shortcuts import render
 
 # Create your views here.
+
+def default(request):
+    return render(request, 'default.html',)
--- iap/urls.py
+++ iap/urls.py
@@ -17,5 +17,6 @@ from django.conf.urls import url
 from django.contrib import admin
 
 urlpatterns = [
+    url(r'^giap/', include('giap.urls'))
     url(r'^admin/', admin.site.urls),
 ]
