8f6f54ff64761078ae3e2ce059996d3116d7d5a3
Daniel Fonseca Alves <dalrafgmail.com>
2017-08-31 11:38:22 -0300

    Definido modelo da central

--- giap/models.py
+++ giap/models.py
@@ -4,3 +4,11 @@ from __future__ import unicode_literals
 from django.db import models
 
 # Create your models here.
+
+class cooperativa(models.Model):
+    sigla_cooperativa = models.CharField('Nome',max_length=50,)
+    numcooperativa = models.IntegerField('Número', max_length=6)
+    sigla_central = models.CharField('Nome',max_length=50,)
+    numcentral = models.IntegerField('Número', max_length=6)
+    
+
