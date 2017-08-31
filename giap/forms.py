5fa5af88b3b79f685edfc11a0425dc05d2bba033
Daniel Fonseca Alves <dalrafgmail.com>
2017-08-31 17:13:45 -0300

    Definido formset e formsadd

--- giap/forms.py
+++ giap/forms.py
@@ -1,19 +1,34 @@
-from django.forms import modelform_factory, modelformset_factory
+from django.forms import modelform_factory, modelform_factory, modelformset_factory
 from giap.models import cliente, cooperativa, central, pa
+
 
 
 formaddcliente = modelform_factory(cliente,
     fields=("pa", "tipodepessoa", "numcpfcpnj", "nome_cliente"),
     )
 
-formaddcentral = modelformset_factory(central,
+formsaddcentral = modelform_factory(central,
     fields=("sigla_central", "numcentral",),
     )
 
-formaddcooperativa = modelformset_factory(cooperativa,
+formsetcentral = modelformset_factory(central,
+    fields=("sigla_central", "numcentral",),
+    )
+
+
+formaddcooperativa = modelform_factory(cooperativa,
     fields=("sigla_cooperativa", "numcooperativa","central"),
     )
 
-formaddpa = modelformset_factory(pa,
+formsetcooperativa = modelformset_factory(cooperativa,
+    fields=("sigla_cooperativa", "numcooperativa","central"),
+    )
+
+formaddpa = modelform_factory(pa,
     fields=("sigla_pa", "numpa", "cooperativa"),
     )
+
+formsetpa = modelformset_factory(pa,
+    fields=("sigla_pa", "numpa", "cooperativa"),
+    )
+
