b72ef945209be1fb628cfb25685c2d8d13a0aeab
Daniel Fonseca Alves <dalrafgmail.com>
2017-08-31 11:39:34 -0300

    Melhoria na descricao

--- giap/models.py
+++ giap/models.py
@@ -6,9 +6,9 @@ from django.db import models
 # Create your models here.
 
 class cooperativa(models.Model):
-    sigla_cooperativa = models.CharField('Nome',max_length=50,)
-    numcooperativa = models.IntegerField('Número', max_length=6)
-    sigla_central = models.CharField('Nome',max_length=50,)
-    numcentral = models.IntegerField('Número', max_length=6)
+    sigla_cooperativa = models.CharField('Nome da Cooperativa',max_length=50,)
+    numcooperativa = models.IntegerField('Número da Cooperativa', max_length=6)
+    sigla_central = models.CharField('Nome da Central',max_length=50,)
+    numcentral = models.IntegerField('Numero da Central', max_length=6)
     
 
