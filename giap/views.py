0c61cdeb76fd421eff6277639c333032b5b5df28
Daniel Fonseca Alves <dalrafgmail.com>
2017-09-01 16:03:29 -0300

    Correcao de adicionar novo

--- giap/views.py
+++ giap/views.py
@@ -18,25 +18,27 @@ def editaddcentral(request, id=None):
     if request.method == 'POST':
         form = formcentral(request.POST)
         if form.is_valid():
-            if request.session['id']:
+            if request.session['id'] != 'new':
                 inst = form.save(commit=False)
                 inst.id = request.session['id']
                 inst.save()
             else:
                 form.save()
+        request.session['id'] == ''
         return redirect('listarcentral',)
-    if id:
+    if request.method == 'GET' and id:
         if id == 'new':
+            request.session['id'] = 'new'
             form = formcentral()
         else:
             objeto = get_object_or_404(central, id=id)
             form = formcentral(request.POST or None, instance=objeto)
             request.session['id'] = id       
-    return render(request, 'editaddcentral.html',
-    {
-        'id': id,
-        'form': form,
-        })
+        return render(request, 'editaddcentral.html',
+        {
+            'id': id,
+            'form': form,
+            })
 
 def deletarcentral(request,id):
     central.objects.filter(id=id).delete()
