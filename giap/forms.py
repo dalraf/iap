4a4ee7c756314fbb230fd48d3e6ab39c3dce4860
Daniel Fonseca Alves <dalraf@gmail.com>
2017-08-31 21:23:36 -0300

    Correcoes

--- giap/forms.py
+++ giap/forms.py
@@ -1,3 +1,4 @@
+from django import forms
 from django.forms import modelform_factory, modelform_factory, modelformset_factory
 from giap.models import cliente, cooperativa, central, pa
 
@@ -7,9 +8,18 @@ formaddcliente = modelform_factory(cliente,
     fields=("pa", "tipodepessoa", "numcpfcpnj", "nome_cliente"),
     )
 
-formaddcentral = modelform_factory(central,
-    fields=("sigla_central", "numcentral",),
-    )
+class formaddcentral(forms.ModelForm):
+    sigla_central = forms.CharField(
+        required=False,
+        widget=forms.TextInput(attrs={'size':'50', 'maxlength': '50'})
+        )
+    numcentral = forms.CharField(
+        required=False,
+        widget=forms.TextInput(attrs={'size':'5','maxlength': '5'}),
+        )
+    class Meta:
+        model = central
+        fields = ("sigla_central", "numcentral",)
 
 formsetcentral = modelformset_factory(central,
     fields=("sigla_central", "numcentral",),
