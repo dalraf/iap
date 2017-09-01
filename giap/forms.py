3e9c3aeb58d287a05466cb42674721519128290e
Daniel Fonseca Alves <dalraf@gmail.com>
2017-08-31 21:48:39 -0300

    Falta remover o bug do nulo

--- giap/forms.py
+++ giap/forms.py
@@ -1,4 +1,3 @@
-from django import forms
 from django.forms import modelform_factory, modelform_factory, modelformset_factory
 from giap.models import cliente, cooperativa, central, pa
 
@@ -8,18 +7,9 @@ formaddcliente = modelform_factory(cliente,
     fields=("pa", "tipodepessoa", "numcpfcpnj", "nome_cliente"),
     )
 
-class formaddcentral(forms.ModelForm):
-    sigla_central = forms.CharField(
-        required=False,
-        widget=forms.TextInput(attrs={'size':'50', 'maxlength': '50'})
-        )
-    numcentral = forms.CharField(
-        required=False,
-        widget=forms.TextInput(attrs={'size':'5','maxlength': '5'}),
-        )
-    class Meta:
-        model = central
-        fields = ("sigla_central", "numcentral",)
+formaddcentral = modelform_factory(central,
+    fields=("sigla_central", "numcentral",),
+    )
 
 formsetcentral = modelformset_factory(central,
     fields=("sigla_central", "numcentral",),
