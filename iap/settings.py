7f69117ba3c71cd0d1bc8c5f91d2eb3c40b0e4d3
Daniel Fonseca Alves <dalrafgmail.com>
2017-09-04 15:20:42 -0300

    Utlizando crispy para renderizar

--- iap/settings.py
+++ iap/settings.py
@@ -32,7 +32,7 @@ ALLOWED_HOSTS = []
 
 INSTALLED_APPS = [
     'giap',
-    'bootstrap3',
+    'crispy_forms',
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
