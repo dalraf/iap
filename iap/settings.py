d53ebdc50afd62dc9d779fce2394f9730b383283
Daniel Fonseca Alves <dalrafgmail.com>
2017-09-06 11:37:50 -0300

    Correcao de posicionamento, usando boostrap django

--- iap/settings.py
+++ iap/settings.py
@@ -32,7 +32,7 @@ ALLOWED_HOSTS = []
 
 INSTALLED_APPS = [
     'giap',
-    'crispy_forms',
+    'bootstrap3',
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
