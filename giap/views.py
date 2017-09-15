efd9ad1315e151206e5a50c296a31ccc6fc0eac1
Daniel Fonseca Alves <dalrafgmail.com>
2017-09-15 15:54:43 -0300

    Correcao

--- giap/views.py
+++ giap/views.py
@@ -284,7 +284,6 @@ def listartransacao(request):
         j[transacao._meta.get_field('cliente').verbose_name] = cliente.objects.get(id=idcliente).nome_cliente
         j[transacao._meta.get_field('produto').verbose_name] = produtodict[i['produto']]
         j[transacao._meta.get_field('grupo').verbose_name] = grupodict[i['grupo']]
-        j[transacao._meta.get_field('vencimento').verbose_name] = i['vencimento']
         j['Dias para vencer'] = (i['vencimento'] - timezone.now().date()).days
         j[transacao._meta.get_field('usuario').verbose_name] = i['usuario']
         j[transacao._meta.get_field('data').verbose_name] = i['data']
