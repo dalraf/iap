ec201493cf8823d1174cad3c98222c1f07ee5980
Daniel Fonseca Alves <dalrafgmail.com>
2017-10-25 15:24:44 -0200

    Adicionado table2

--- iap/settings.py
+++ iap/settings.py
@@ -33,6 +33,7 @@ ALLOWED_HOSTS = []
 INSTALLED_APPS = [
     'giap',
     'bootstrap3',
+    'django_tables2',
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
@@ -64,6 +65,7 @@ TEMPLATES = [
                 'django.template.context_processors.request',
                 'django.contrib.auth.context_processors.auth',
                 'django.contrib.messages.context_processors.messages',
+                'django.template.context_processors.request',
             ],
         },
     },
